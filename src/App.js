60444cf3469213f7ad6c904855d3d1c5d06b2e52
Naveed Ali <naveedalilodro.xwave@gmail.com>
2024-11-05 15:20:15 -1200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import FormComponent from './components/form/FormComponent';
+function App() {
+  return (
+   
+
+ <>
+
+  <FormComponent/>  
+  
+   </>
+  );
+}
+
+export default App;
